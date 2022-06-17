a40727b654f18cd00201d940a5973316d3babd91
MrStonkus <valdas@stonkus.lt>
2022-06-17 17:31:35 +0300

    add two pages layout

--- src/pages/Layout/LayoutTwoPages/LayoutTwoPages.jsx
+++ src/pages/Layout/LayoutTwoPages/LayoutTwoPages.jsx
@@ -1,82 +1,75 @@
 import { Outlet } from 'react-router-dom'
 import { Container } from '@mui/material'
-// import Header from '../Header'
-// import Footer from '../Footer'
-import Paper from '@mui/material/Paper'
-import Box from '@mui/material/Box'
+import { useEffect, useRef, useState } from 'react'
 import WelcomePage from '../../WelcomePage'
 import ResumePage from '../../ResumePage'
+import './styles.scss'
 
-const LayoutTwoPages = () => (
-    <Container
-        // fixed
-        disableGutters
-        maxWidth="xl"
-        sx={{
-            bgcolor: 'red',
-            position: 'relative',
-            top: '0px'
-            // height: '100vh',
-        }}
-    >
+const LayoutTwoPages = () => {
+    const [isLeftPageActive, setLeftPageActive] = useState(true)
+    const leftPageEl = useRef(null)
+    const rightPageEl = useRef(null)
+
+    useEffect(
+        () => {
+            if (isLeftPageActive) {
+                leftPageEl.current.classList.add('page-left-fadeIn')
+                rightPageEl.current.classList.add('page-right-fadeOut')
+                setTimeout(() => {
+                    rightPageEl.current.classList.remove('page-right-fadeIn')
+                }, 500)
+            } else {
+                leftPageEl.current.classList.remove('page-left-fadeIn')
+                rightPageEl.current.classList.remove('page-right-fadeOut')
+                rightPageEl.current.classList.add('page-right-fadeIn')
+            }
+        },
+        [isLeftPageActive]
+    )
+
+    const clickHandler = (action) => {
+        if (action === 'activateFirstPage') {
+            setLeftPageActive(true)
+        } else {
+            setLeftPageActive(false)
+        }
+    }
+    return (
         <Container
             disableGutters
             maxWidth="lg"
             sx={{
-                // bgcolor: 'white',
-                position: 'absolute',
-                left: '0px',
-                top: '50px',
-                // width: '70%',
-                // height: '100vh',
-                // border: '1px solid black',
-                zIndex: '1'
+                bgcolor: 'red',
+                position: 'relative',
+                top: '0px'
             }}
         >
-            <Box
+            <Container
+                className="page page-left"
+                ref={leftPageEl}
+                onClick={() => clickHandler('activateFirstPage')}
+                disableGutters
+                maxWidth="md"
                 sx={{
-                    display: 'flex',
-                    // NOTE kai užsipildys lapas, užkomentuoti
-                    '& > :not(style)': {
-                        width: '100%'
-                    }
-                    // height: '100vh'
+                    boxShadow: 24
                 }}
             >
-                <Paper elevation={24} square>
-                    <WelcomePage />
-                </Paper>
-            </Box>
-        </Container>
-        <Container
-            disableGutters
-            maxWidth="lg"
-            sx={{
-                bgcolor: 'white',
-                position: 'absolute',
-                right: '0px',
-                top: '-1px',
-                width: '50%',
-                zIndex: '0'
-            }}
-        >
-            <Box
+                <WelcomePage />
+            </Container>
+            <Container
+                className="page page-right"
+                ref={rightPageEl}
+                onClick={() => clickHandler('')}
+                disableGutters
+                maxWidth="md"
                 sx={{
-                    display: 'flex',
-                    // NOTE kai užsipildys lapas, užkomentuoti
-                    '& > :not(style)': {
-                        width: '100%'
-                    }
-                    // height: '80vh'
+                    boxShadow: 24
                 }}
             >
-                <Paper elevation={24} square>
-                    <ResumePage />
-                </Paper>
-            </Box>
+                <ResumePage />
+            </Container>
+
         </Container>
-
-    </Container>
-)
-
+    )
+}
 export default LayoutTwoPages
