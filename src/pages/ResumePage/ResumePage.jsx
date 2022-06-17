cb2e87462bf04e7b9f089f34f11efee547787576
MrStonkus <valdas@stonkus.lt>
2022-06-17 03:41:31 +0300

    create LayoutTwoPages

---
+++ src/pages/ResumePage/ResumePage.jsx
@@ -0,0 +1,6 @@
+const ResumePage = () => (
+    <div>
+        <h1>Welcome to ResumePage!</h1>
+    </div>
+)
+export default ResumePage
---
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -0,0 +1,6 @@
+const WelcomePage = () => (
+    <div>
+        <h1>WelcomePage!</h1>
+    </div>
+)
+export default WelcomePage
