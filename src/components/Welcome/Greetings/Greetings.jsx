8cf63f7deee67cd3c8195ed04a8a7703d7155236
MrStonkus <valdas@stonkus.lt>
2022-06-21 20:15:16 +0300

    create welcome component

---
+++ src/components/Welcome/Greetings/Greetings.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+import './styles.scss'
+import { Button, Stack, Typography } from '@mui/material'
+
+const Greetings = () => (
+    <>
+        <Typography variant="h4" component="h6" sx={{ fontWeight: 'bold' }}>
+            Hi, I am Valdas and I am a
+            {' '}
+            <span className="title--dark">&nbsp;Front-end Developer.</span>
+        </Typography>
+        <Typography variant="h5" component="h5" mt={2} sx={{ fontSize: '1rem' }}>
+            I am based in Šiauliai, Lithuania. I have more that 1 year experience of creating web applications. I am open for new opportunities and interesting projects.
+        </Typography>
+        <Stack direction="row" spacing={2} mt={2} justifyContent="center">
+            <Button variant="contained" bgcolor="black">Contact me</Button>
+            <Button variant="outlined">Check my work</Button>
+        </Stack>
+    </>
+)
+
+export default Greetings
