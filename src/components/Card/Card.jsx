4394148258c6e670b7f8fcb3021d2a572ef8d0e7
MrStonkus <valdas@stonkus.lt>
2022-07-10 12:07:40 +0300

    create preview component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,60 +5,72 @@ import {
 import GitHubIcon from '@mui/icons-material/GitHub'
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye'
 import { useState } from 'react'
+import Preview from '../Preview'
 
 const Card = ({ project, alwaysShowActionButtons = false }) => {
     const [isMouseOver, setIsMouseOver] = useState(false)
+    const [showPreview, setShowPreview] = useState(false)
 
     return (
-        <CardMui
-            onMouseEnter={() => setIsMouseOver(true)}
-            onMouseLeave={() => setIsMouseOver(false)}
-            sx={{
-                display: 'flex',
-                minHeight: '100%',
-                maxWidth: 345,
-                flexDirection: 'column'
-            }}
-        >
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    height="140"
-                    image={project.imgUrl}
-                    alt={project.imgAlt}
-                />
-                <CardContent sx={{ pb: '0px' }}>
-                    <Typography gutterBottom variant="h6" component="div">
-                        {project.title}
-                    </Typography>
-                    <Box mb={2}>
-                        <Typography variant="body2" color="text.secondary">
-                            {project.shortDescription}
-                        </Typography>
-                    </Box>
-                    <Typography align="right" variant="overline" color="text.secondary">
-                        {project.tags.map((t, i) => (
-                            <span key={i}>
-                                {t}
-                                {' '}
-                            </span>
-                        ))}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-            <Box sx={{
-                marginTop: 'auto'
-            }}
+        <>
+            <CardMui
+                onMouseEnter={() => setIsMouseOver(true)}
+                onMouseLeave={() => setIsMouseOver(false)}
+                sx={{
+                    display: 'flex',
+                    minHeight: '100%',
+                    maxWidth: 345,
+                    flexDirection: 'column'
+                }}
             >
-                <CardActions sx={{ padding: '0 16px 16px 16px', visibility: alwaysShowActionButtons ? 'visible' : isMouseOver ? 'visible' : 'hidden' }}>
-                    <Button variant="contained" color="primary" startIcon={<RemoveRedEyeIcon />} size="small" href={project.btnActionUrl}>
-                        {project.btnActionTitle}
-                    </Button>
-                    <Button variant="text" color="secondary" startIcon={<GitHubIcon />} href={project.btnRepoUrl} target="_blank">Github repo</Button>
-                </CardActions>
-            </Box>
-
-        </CardMui>
+                <CardActionArea onClick={() => setShowPreview(true)}>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image={project.imgUrl}
+                        alt={project.imgAlt}
+                    />
+                    <CardContent sx={{ pb: '0px' }}>
+                        <Typography gutterBottom variant="h6" component="div">
+                            {project.title}
+                        </Typography>
+                        <Box mb={2}>
+                            <Typography variant="body2" color="text.secondary">
+                                {project.shortDescription}
+                            </Typography>
+                        </Box>
+                        <Typography align="right" variant="overline" color="text.secondary">
+                            {project.tags.map((t, i) => (
+                                <span key={i}>
+                                    {t}
+                                    {' '}
+                                </span>
+                            ))}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <Box sx={{
+                    marginTop: 'auto'
+                }}
+                >
+                    <CardActions sx={{ padding: '0 16px 16px 16px', visibility: alwaysShowActionButtons ? 'visible' : isMouseOver ? 'visible' : 'hidden' }}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            startIcon={(
+                                <RemoveRedEyeIcon />
+                            )}
+                            size="small"
+                            href={project.btnActionUrl}
+                        >
+                            {project.btnActionTitle}
+                        </Button>
+                        <Button variant="text" color="secondary" startIcon={<GitHubIcon />} href={project.btnRepoUrl} target="_blank">Github repo</Button>
+                    </CardActions>
+                </Box>
+            </CardMui>
+            <Preview show={showPreview} setShow={setShowPreview} project={project} />
+        </>
     )
 }
 export default Card
---
+++ src/components/Preview/Preview.jsx
@@ -0,0 +1,74 @@
+import { Box, Typography } from '@mui/material'
+import Button from '@mui/material/Button'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
+import { useEffect, useRef, useState } from 'react'
+import Images from './Images'
+
+const Preview = ({ project, show, setShow }) => {
+    if (!show) return null
+    const [scroll, setScroll] = useState('paper')
+
+    const handleClickOpen = (scrollType) => () => {
+        setShow(true)
+        setScroll(scrollType)
+    }
+
+    const handleClose = () => {
+        setShow(false)
+    }
+
+    const descriptionElementRef = useRef(null)
+    useEffect(() => {
+        if (show) {
+            const { current: descriptionElement } = descriptionElementRef
+            if (descriptionElement !== null) {
+                descriptionElement.focus()
+            }
+        }
+    }, [show])
+
+    return (
+        <div>
+            <Button onClick={handleClickOpen('paper')}>scroll=paper</Button>
+            <Button onClick={handleClickOpen('body')}>scroll=body</Button>
+            <Dialog
+                open={show}
+                onClose={handleClose}
+                scroll={scroll}
+                aria-labelledby="scroll-dialog-title"
+                aria-describedby="scroll-dialog-description"
+            >
+                <DialogTitle id="scroll-dialog-title">{project.title}</DialogTitle>
+                <DialogContent dividers={scroll === 'paper'}>
+                    <DialogContentText
+                        id="scroll-dialog-description"
+                        ref={descriptionElementRef}
+                        tabIndex={-1}
+                    />
+                    <Box p={2}>
+                        <Typography variant="h6" color="text.secondary">Problem</Typography>
+                        <Typography variant="body" color="text.secondary">{project.problem}</Typography>
+                    </Box>
+                    <Box p={2}>
+                        <Typography variant="h6" color="text.secondary">Solution</Typography>
+                        <Typography variant="body" color="text.secondary">{project.solution}</Typography>
+                    </Box>
+                    <Box p={2}>
+                        <Typography variant="h6" color="text.secondary">Implementation</Typography>
+                        <Typography variant="body" color="text.secondary">{project.implementation}</Typography>
+                    </Box>
+                    <Images pictures={project.pictures} />
+                </DialogContent>
+                <DialogActions>
+                    <Button color="secondary" onClick={handleClose}>Close</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    )
+}
+
+export default Preview
