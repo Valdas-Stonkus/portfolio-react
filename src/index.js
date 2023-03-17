a0446a0e314283e4e08b7b5c5628b85d195424a6
MrStonkus <valdas@stonkus.lt>
2023-03-17 17:19:29 +0200

    hide horizontal bar

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const theme = createTheme({
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
-        <div className="wrapper">
+        <div className="body-wrapper">
             <CssBaseline />
             <ThemeProvider theme={theme}>
                 <BrowserRouter>
